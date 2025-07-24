a720a3f880cac3af434397b4dd1a23c4ab1b2fe3
LuiMagno <luicosta3@gmail.com>
2025-07-23 23:46:52 -0300

    Adicionado novas features do widget - novas caixas, filtros e aplicação do cosine_similarity

--- utils/vetorizacao.py
+++ utils/vetorizacao.py
@@ -14,28 +14,6 @@ def vetorizar_perfis(df: pd.DataFrame) -> pd.DataFrame:
     """
     df = df.copy()
 
-    # 🟡 Mapeia tempo de experiência para números
-    mapa_experiencia = {
-        "Menos de 1 ano": 0.5,
-        "de 1 a 2 anos": 1.5,
-        "de 2 a 3 anos": 2.5,
-        "de 3 a 4 anos": 3.5,
-        "de 4 a 5 anos": 4.5,
-        "de 5 a 6 anos": 5.5,
-        "de 6 a 7 anos": 6.5,
-        "de 7 a 8 anos": 7.5,
-        "de 8 a 9 anos": 8.5,
-        "de 9 a 10 anos": 9.5,
-        "Mais de 10 anos": 12
-    }
-
-    if "2.i_tempo_de_experiencia_em_dados" in df.columns:
-        df["experiencia_dados_anos"] = df["2.i_tempo_de_experiencia_em_dados"].map(mapa_experiencia)
-    if "2.j_tempo_de_experiencia_em_ti" in df.columns:
-        df["experiencia_ti_anos"] = df["2.j_tempo_de_experiencia_em_ti"].map(mapa_experiencia)
-
-    #  Remove colunas textuais que foram convertidas
-    df = df.drop(columns=["2.i_tempo_de_experiencia_em_dados", "2.j_tempo_de_experiencia_em_ti"], errors="ignore")
 
     #  Converte colunas categóricas para dummies
     df_dummies = pd.get_dummies(df, drop_first=False)
